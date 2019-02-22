f90f3239404d431729044210f7f198f47896aaa1
linhdinh253 <47020296+linhdinh253@users.noreply.github.com>
2019-02-22 15:29:09 +0700

    Add files via upload

---
+++ MainPage.php
@@ -0,0 +1,399 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="icon" href="https://ih1.redbubble.net/image.106779121.2467/flat,800x800,075,f.u11.jpg">
+        <script type="text/javascript">
+var rev = "fwd";
+function titlebar(val){
+var msg = " WELCOME ♡ ";
+var res = " ";
+var speed = 100;
+var pos = val;
+msg = " WELCOME ♡ ";
+var le = msg.length;
+if(rev == "fwd"){
+if(pos < le){
+pos = pos+1;
+scroll = msg.substr(0,pos);
+document.title = scroll;
+timer = window.setTimeout("titlebar("+pos+")",speed);
+}else{
+rev = "bwd";
+timer = window.setTimeout("titlebar("+pos+")",speed);
+}}else{
+if(pos > 0){
+pos = pos-1;
+var ale = le-pos;
+scrol = msg.substr(ale,le);
+document.title = scrol;
+timer = window.setTimeout("titlebar("+pos+")",speed);
+}else{
+rev = "fwd";
+timer = window.setTimeout("titlebar("+pos+")",speed);
+} }}
+titlebar(0);
+</script>
+        
+
+
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <style>
+body {background: #ffffff url("http://irigare.com/wp-content/uploads/marble-rose-gold-watercolour-coral-blush-brushstroke-desktop-background-tech-backgrounds-and-gorgeous-photos-with-desk-5.jpg") no-repeat center center fixed; 
+    background-size: cover;
+    }
+        .topic{
+            background: white;
+            width: 600px;
+            height: 50px;
+            font-color: black;
+            float: center;
+            padding-top: -10px;
+            margin: 0px 0px 50px 400px;
+            font-family: calibri;
+
+        }
+        .group1:after{
+            clear: both;
+            content: ".";
+            display: block;
+            width: 0px;
+            height: 0px;
+        }
+
+        .left{
+            border: solid 6px white;
+            width: 450px;
+            height: 600px;
+            float: center;
+            margin: 0px 100px 50px 480px;
+            padding: center;
+            padding-left: 5px;
+            padding-right: 5px;
+            text-align: center;
+            font-family: courier;
+        }
+        .right{
+
+            border: solid 6px white;
+            width: 450px;
+            height: 650px;
+            float: center;
+         padding: center;
+            padding-left: 5px;
+            padding-right: 5px;
+            text-align: center;
+            font-family: courier;
+            margin: 0 0 50px 680px;
+
+        }
+        .group2{
+            border: solid 6px white;
+            width: 450px;
+            height: 680px;
+            margin: 0 0 0 480px;
+            padding: center;
+            padding-left: 5px;
+            padding-right: 5px;
+            text-align: center;
+            font-family: courier;
+        }
+
+       
+        </style>
+    </head>
+    <body>
+        <div class="topic">
+            <center><h2>RECOMMENDED RESTAURANTS IN HATYAI</h2></center></div>
+        <div class="group1">
+            <div class="left">
+                        <h2>Homeless Bar and Grill</h2>
+
+                <img src="https://media-cdn.tripadvisor.com/media/photo-s/0a/03/fe/9f/homeless-bar-and-grill.jpg" width="200px" height="150px">
+                <p><strong>This is a hidden gem in Hat Yai. A really nice place to go with friends or a date. I appreciated the atmosphere and the general image of the place. Everyone working there was friendly and outgoing. The pizza was delicious. The drinks were strong.</p></strong>
+                <a href="https://www.tripadvisor.com/Restaurant_Review-g297921-d9727859-Reviews-Homeless_Bar_and_Grill-Hat_Yai_Songkhla_Province.html" target="_blank"><h3>Click Me!!!</h3></a>
+                <!DOCTYPE html>
+<html>
+<head>
+
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<style>
+.checked {
+    color: orange;
+}
+</style>
+</head>
+<body>
+
+<span class="fa fa-star checked"></span>
+<span class="fa fa-star checked"></span>
+<span class="fa fa-star checked"></span>
+<span class="fa fa-star checked"></span>
+<span class="fa fa-star"></span><br>
+
+</body>
+</html>
+<!DOCTYPE html>
+<html>
+<head>
+<title>ReviewTable</title>
+
+</head>
+<body>
+<br>
+<table style="width:50%;border:2px solid;padding:1px;border-color: white; position: relative;right: -50px;">
+<tr bgcolor="tomato">
+<th width="150" height="20"><font color="white">Suggestion</th>
+<th width="100" height="20"><font color="white">Good</th>
+<th width="50" height="20"><font color="white">Satisfactory</th>
+<th width="50" height="20"><font color="white">Needs Improvement</th>
+</tr>
+
+<tr bgcolor="pink">
+<td width="150" height="20">Appearance</td>
+<td width="150" height="20"></td>
+<td width="150" height="20"><center>*</center><td>
+</tr>
+
+<tr bgcolor="pink">
+<td width="150" height="20">Taste</td>
+<td width="150" height="20"></td>
+<td width="150" height="20"><center>*</center></td>
+<td width="150" height="20"></td>
+</tr>
+
+<tr bgcolor="pink">
+<td width="150" height="20">Worth it?</td>
+<td width="150" height="20"></td>
+<td width="150" height="20"><center>*</center></td>
+<td width="150" height="20"></td>
+</tr>
+
+<tr bgcolor="pink">
+<td width="150" height="20">Timing</td>
+<td width="150" height="20"><center>*</center></td>
+<td width="150" height="20"></td>
+<td width="150" height="20"></td>
+</tr>
+
+</table>
+
+</body>
+</html>
+            </div>
+            <div class="right" style="position: relative; left: -200px">
+                <center><h1> A LITTLE</h1></center>
+                 <img src="https://scontent.fbkk5-8.fna.fbcdn.net/v/t1.15752-9/32219162_1923198094419658_3784388576058277888_n.png?_nc_cat=0&oh=ffef691325ae0b3bf45d7600baa4360a&oe=5B99ABCD" width="200px" height="150px">
+                <p><strong>A Little is different from conventional Cantonese style dim sum restaurant that features a minimalist modern architecture outlook. The food taste is above average and served in clean, prompt attitude. Recommended for "Yum Cha" if you looking for some different Dim Sum and you have a little more budget in hand.</p></strong>
+                <a href="https://www.tripadvisor.com/Restaurant_Review-g297921-d11892944-Reviews-A_LITTLE-Hat_Yai_Songkhla_Province.html" target="_blank"><h3>Click Me!!!</h3></a>
+                 <!DOCTYPE html>
+<html>
+<head>
+
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<style>
+.checked {
+    color: orange;
+}
+</style>
+</head>
+<body>
+
+<span class="fa fa-star checked"></span>
+<span class="fa fa-star checked"></span>
+<span class="fa fa-star checked"></span>
+<span class="fa fa-star checked"></span>
+<span class="fa fa-star"></span><br>
+
+</body>
+</html>
+<!DOCTYPE html>
+<html>
+<head>
+<title>ReviewTable</title>
+
+</head>
+<body>
+
+<br>
+<table style="width:50%;border:2px solid;padding:1px;border-color: white; position: relative;right: -50px;">
+<tr bgcolor="tomato">
+<th width="150" height="20"><font color="white">Suggestion</th>
+<th width="100" height="20"><font color="white">Good</th>
+<th width="50" height="20"><font color="white">Satisfactory</th>
+<th width="50" height="20"><font color="white">Needs Improvement</th>
+</tr>
+
+<tr bgcolor="pink">
+<td width="150" height="20">Appearance</td>
+<td width="150" height="20"></td>
+<td width="150" height="20"><center>*</center><td>
+</tr>
+
+<tr bgcolor="pink">
+<td width="150" height="20">Taste</td>
+<td width="150" height="20"></td>
+<td width="150" height="20"><center>*</center></td>
+<td width="150" height="20"></td>
+</tr>
+
+<tr bgcolor="pink">
+<td width="150" height="20">Worth it?</td>
+<td width="150" height="20"></td>
+<td width="150" height="20"><center>*</center></td>
+<td width="150" height="20"></td>
+</tr>
+
+<tr bgcolor="pink">
+<td width="150" height="20">Timing</td>
+<td width="150" height="20"><center>*</center></td>
+<td width="150" height="20"></td>
+<td width="150" height="20"></td>
+</tr>
+
+</table>
+
+</body>
+</html>
+            </div>
+        </div>
+        <div class="group2">
+             <center><h1> I'M HOUSE</h1></center>
+                 <img src="https://media-cdn.tripadvisor.com/media/photo-s/08/d3/8f/12/im-house.jpg" width="200px" height="150px">
+                <p><strong>"Life is too short to drink bad coffee" is the philosophy of Im House, a cool and stylish tropical modern-style coffee shop. Also, it has unique ambience as the coffee scents from all around the world are here at the intersections of Phetkasem Rd., Wongwanich Rd., and Prachayindee Rd. Besides it is the real homemade bakery shop with tasty but simple menus served everyday 10am-7pm.</p></strong>
+                <a href="https://www.tripadvisor.com/Restaurant_Review-g297921-d8580998-Reviews-Im_house-Hat_Yai_Songkhla_Province.html" target="_blank"><h3>Click Me!!!</h3></a>
+                 <!DOCTYPE html>
+<html>
+<head>
+
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<style>
+.checked {
+    color: orange;
+}
+</style>
+</head>
+<body>
+
+<span class="fa fa-star checked"></span>
+<span class="fa fa-star checked"></span>
+<span class="fa fa-star checked"></span>
+<span class="fa fa-star "></span>
+<span class="fa fa-star"></span><br>
+
+<!DOCTYPE html>
+<html>
+<head>
+<title>ReviewTable</title>
+
+</head>
+<body>
+<br>
+<table style="width:50%;border:2px solid;padding:1px;border-color: white;position: relative;right: -50px;">
+<tr bgcolor="tomato">
+<th width="150" height="20"><font color="white">Suggestion</th>
+<th width="100" height="20"><font color="white">Good</th>
+<th width="50" height="20"><font color="white">Satisfactory</th>
+<th width="50" height="20"><font color="white">Needs Improvement</th>
+</tr>
+
+<tr bgcolor="pink">
+<td width="150" height="20">Appearance</td>
+<td width="150" height="20"></td>
+<td width="150" height="20"><center>*</center><td>
+</tr>
+
+<tr bgcolor="pink">
+<td width="150" height="20">Taste</td>
+<td width="150" height="20"></td>
+<td width="150" height="20"><center>*</center></td>
+<td width="150" height="20"></td>
+</tr>
+
+<tr bgcolor="pink">
+<td width="150" height="20">Worth it?</td>
+<td width="150" height="20"></td>
+<td width="150" height="20"><center>*</center></td>
+<td width="150" height="20"></td>
+</tr>
+
+<tr bgcolor="pink">
+<td width="150" height="20">Timing</td>
+<td width="150" height="20"><center>*</center></td>
+<td width="150" height="20"></td>
+<td width="150" height="20"></td>
+</tr>
+
+</table>
+
+</body>
+</html>
+
+
+</body>
+</html>
+        </div>
+
+
+
+<!DOCTYPE html>
+<html>
+<head>
+   <title>food survey</title>
+
+</head>
+<body>
+  <h1><center>Food survey</center></h1>
+
+  <form action="foodsurvey.php>" method="post"><center>
+ How often do you eat out a lot?:<br><br>
+    <select name="outside">
+        <option value="Always">Always</option>
+        <option value="Occasionally">Occasionally</option>
+        <option value="Rarely">Rarely</option>
+    </select><br><br>
+
+ What meal do you usually eat out?:<br><br>
+    <select name="meal">
+        <option value="Breakfast">Breakfast</option>
+        <option value="Lunch">Lunch</option>
+        <option value="Dinner">Dinner</option>
+    </select><br><br>
+
+  What kind of food do you like?:<br><br>
+    <select name="national">
+        <option value="Thai">Thai</option>
+        <option value="Chinese">Chinese</option>
+        <option value="Japanaes">Japanaes</option>
+        <option value="Western">Western</option>
+        <option value="Korean">Korean</option>
+        <option value="Others">Others</option>
+    </select><br><br>
+
+ What kind of restuarant do you go to?:<br><br>
+    <select name="type">
+        <option value="Buffet">Buffet</option>
+        <option value="Main course / One dish">Main course / One dish</option>
+        <option value="Fast food">Fast food</option>
+        <option value="Dessert">Dessert</option>
+    </select><br><br>
+
+  Age:<br><br>
+  <input type="text" name="age" >
+  <br><br>
+
+  Email:<br><br>
+  <input type="text" name="email" >
+  <br><br>
+
+  <input type="submit" value="Submit">
+  <br><br> 
+</center></form> 
+
+</body>
+</html>
+
+<h1><center>THANK YOU ♡</center></h1>
+
+
+    </body>
+</html>
---
+++ foodsurvey.php
@@ -0,0 +1,88 @@
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "sii";
+
+$conn = new mysqli($servername , $username , $password , $dbname);
+
+if ($conn->connect_error) {
+ die("Connection failed: " . $conn->connect_error);
+} 
+    
+   if($_SERVER["REQUEST_METHOD"] == "POST") {
+
+      $sql= "SELECT dept_id,dept_name from department";
+      $stmt = $conn->prepare("INSERT INTO foodsurvey(outside, meal, national, type, age, email) VALUES (?, ?, ?, ?, ?, ?)");
+      $stmt->bind_param("ssssss", $outside, $meal, $national, $type, $age, $email);
+
+      $outside = $_POST['outside'];
+      $meal = $_POST['meal']; 
+      $national = $_POST['national'];
+      $type = $_POST['type'];
+      $age = $_POST['age'];
+      $email = $_POST['email'];
+
+      $result = $conn->query($sql);
+      $stmt->execute();
+   }
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+   <title>food survey</title>
+
+</head>
+<body>
+  <h1>food survey</h1>
+
+  <form method="post">
+ How often do you eat out a lot?:<br>
+    <select name="outside">
+        <option value="Always">Always</option>
+        <option value="Occasionally">Occasionally</option>
+        <option value="Rarely">Rarely</option>
+    </select><br>
+
+ What meal do you usually eat out?:<br>
+    <select name="meal">
+        <option value="Breakfast">Breakfast</option>
+        <option value="Lunch">Lunch</option>
+        <option value="Dinner">Dinner</option>
+    </select><br>
+
+  What kind of food do you like?:<br>
+    <select name="national">
+        <option value="Thai">Thai</option>
+        <option value="Chinese">Chinese</option>
+        <option value="Japanaes">Japanaes</option>
+        <option value="Western">Western</option>
+        <option value="Korean">Korean</option>
+        <option value="Others">Others</option>
+    </select><br>
+
+ What kind of restuarant do you go to?:<br>
+    <select name="type">
+        <option value="Buffet">Buffet</option>
+        <option value="Main course / One dish">Main course / One dish</option>
+        <option value="Fast food">Fast food</option>
+        <option value="Dessert">Dessert</option>
+    </select><br>
+
+  Age:<br>
+  <input type="text" name="age" >
+  <br>
+
+  Email:<br>
+  <input type="text" name="email" >
+  <br>
+
+  <input type="submit" value="Submit">
+  <br> 
+</form> 
+
+</body>
+</html>
